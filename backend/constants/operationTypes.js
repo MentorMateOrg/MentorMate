5021caa2016a5c8175a325fd04e57e89347c4fd4
Ebenezer Kobina Acquah <ebenezeracquah501@gmail.com>
2025-07-23 14:27:06 -0700

    Featue: Built a simple Myers algorithm to strenghten Implementation of Operational Transformation

--- backend/constants/operationTypes.js
+++ backend/constants/operationTypes.js
@@ -1,6 +1,4 @@
 // Create backend/constants/operationTypes.js
-export const OP_RETAIN = 'retain';
-export const OP_INSERT = 'insert';
-export const OP_DELETE = 'delete';
-
-
+export const OP_RETAIN = "retain";
+export const OP_INSERT = "insert";
+export const OP_DELETE = "delete";
